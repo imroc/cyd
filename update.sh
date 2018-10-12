056eaf38bb68ebe38bbac8acd09ae97fcef157a4
roc <roc@imroc.io>
2018-10-13 01:09:43 +0800

    rebuilding site 2018年10月13日 星期六 01时09分43秒 CST

---
+++ update.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+msg="rebuilding site `date`"
+if [ $# -eq 1  ]
+    then msg="$1"
+fi
+
+git add .
+git commit -m "$msg"
+git push
